6b70923a08077ac37f29472f819aaf9babdfa429
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-16 15:04:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+
+
+
+
+
+
+
+function submitData(name, mail){
+    const bodyTag = document.getElementsByTagName("body");
+    const formData = {
+        name: name,
+        email: mail,
+      };
+    const configurationObject = {
+        method : "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+    
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(object){
+     const newId = object.id
+     bodyTag.innerHTML = `${object.id}`;
+    })
+
+}
